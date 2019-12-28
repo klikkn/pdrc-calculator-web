d88bde183421e1c2e00c67c5a44ed4db8123c1b8
Sergey Perezhogin <klikkn@yandex.ru>
2019-12-28 19:51:31 +0100

    remove unused fields

--- src/containers/Calculation.vue
+++ src/containers/Calculation.vue
@@ -4,44 +4,10 @@
 
     <form v-on:submit.prevent="onSubmit" v-on:reset="onReset">
       <table>
-        <tr>
-          <td>
-            <select v-model="form.make" :disabled="!makes.length" @change="onMakeChange">
-              <option disabled value="null">{{ $t('select.make') }}</option>
-              <option v-for="(make, index) of makes" :key="index" :value="make">{{make.title}}</option>
-            </select>
-          </td>
-        </tr>
 
         <tr>
           <td>
-            <select
-              v-model="form.model"
-              :disabled="!form.make || !models.length"
-              @change="onModelChange"
-            >
-              <option disabled value="null">{{ $t('select.model') }}</option>
-              <option
-                v-for="model of modelsByMakeId(form.make ? form.make.id : null)"
-                :key="model.id"
-                :value="model"
-              >{{model.title}}</option>
-            </select>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <select v-model="form.year" :disabled="!form.model">
-              <option disabled value="null">{{ $t('select.year') }}</option>
-              <option v-for="(year) in getYearsRange(form.model)" :key="year">{{year}}</option>
-            </select>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <select v-model="form.classValue" :disabled="!form.model">
+            <select v-model="form.classValue">
               <option disabled value="null">{{ $t('select.class') }}</option>
               <option v-for="(value, index) of classes" :key="index" :value="value">{{value}}</option>
             </select>
@@ -88,14 +54,11 @@
 </style>
 
 <script>
-import { clone, range, isEmpty } from "ramda";
+import { clone, isEmpty } from "ramda";
 import { mapState, mapGetters, mapActions } from "vuex";
 import { calculate } from "../services/api";
 
 const defaultFormState = {
-  make: null,
-  model: null,
-  year: null,
   classValue: null,
 
   selected: [],
@@ -106,9 +69,7 @@ const defaultFormState = {
 export default {
   mounted: function() {
     if (isEmpty(this.params)) this.getParams();
-    if (isEmpty(this.makes)) this.getMakes();
-
-    this.getParts();
+    if (isEmpty(this.parts))  this.getParts();
   },
 
   data: function() {
@@ -118,16 +79,14 @@ export default {
   computed: {
     ...mapState({
       params: state => state.params,
-      makes: state => state.makes,
-      models: state => state.models,
       parts: state => state.parts
     }),
 
     ...mapGetters(["prices", "classes", "squares", "modelsByMakeId"]),
 
     isSubmitDisabled: function() {
-      const { make, model, classValue } = this.form;
-      return ![make, model, classValue].every(Boolean);
+      const { classValue } = this.form;
+      return ![classValue].every(Boolean);
     },
 
     isEmptyParams: function() {
@@ -138,9 +97,6 @@ export default {
   methods: {
     ...mapActions([
       "getParams",
-      "getMakes",
-      "getModel",
-      "getMakeModels",
       "getParts"
     ]),
 
@@ -163,28 +119,6 @@ export default {
     onReset: function() {
       this.form = clone(defaultFormState);
       this.result = 0;
-    },
-
-    onMakeChange: function() {
-      this.form.model = null;
-      this.form.year = null;
-      this.form.classValue = null;
-
-      this.getMakeModels({ id: this.form.make.id });
-    },
-
-    onModelChange: function() {
-      if (!this.form.model) return;
-      this.form.classValue = this.form.model.class;
-    },
-
-    getYearsRange(model) {
-      if (!model) return [];
-
-      return range(
-        model.from,
-        model.till ? model.till : new Date().getFullYear()
-      );
     }
   }
 };

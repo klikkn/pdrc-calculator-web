f1cea023a27cd86ad31c903813ad9afbaa6e1775
Sergey <klikkn@yandex.ru>
2020-01-05 13:20:59 +0100

    upd font in drowdown

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,11 @@
   width: 290px;
 }
 
+.el-select-dropdown__item {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
+
 .auth {
   display: flex;
   flex-direction: column;
